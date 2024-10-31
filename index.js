cae3bf0194ac2ed8c0353e6478aed1b1d40adad9
Toan Duong <toandt@nexlesoft.com>
2024-10-31 20:27:45 +0700

    sap xep lai code

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const { WebcastPushConnection } = require('tiktok-live-connector');
 
 // Username of someone who is currently live
-let tiktokUsername = "vytoto19";
+let tiktokUsername = "xizy_77";
 
 // Create a new wrapper object and pass the username
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
-// Connect to the chat (await can be used as well)
-tiktokLiveConnection.connect().then(state => {
-    console.info(`Connected to roomId ${state.roomId}`);
-}).catch(err => {
-    console.error('Failed to connect', err);
-})
 
 // ...and more events described in the documentation below
 
@@ -21,6 +15,14 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
     console.log('A new client connected');
+
+
+    // Connect to the chat (await can be used as well)
+    tiktokLiveConnection.connect().then(state => {
+        console.info(`Connected to roomId ${state.roomId}`);
+    }).catch(err => {
+        console.error('Failed to connect', err);
+    })
     // Define the events that you want to handle
     // In this case we listen to chat messages (comments)
     tiktokLiveConnection.on('chat', data => {
