2462852e17da18e644d62a49dca006e3c30d7f82
Toan Duong <toandt@nexlesoft.com>
2024-10-31 20:54:33 +0700

    tam dc bong bong

---
+++ model.js
@@ -0,0 +1,43 @@
+const debug31 = {
+    emotes: [],
+    comment: 'Ko quậy ko phải là maximus',
+    userId: '7144708082043700251',
+    secUid: 'MS4wLjABAAAA5v_F4LdD6Eib1zlI-_ESZar4xPbaBCdqM9pTmH9D44FrVJvmCrExNrs_2R1XkvjY',
+    uniqueId: 'mars_090503',
+    nickname: 'Maximus                  🐰 🐮',
+    profilePictureUrl: 'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cb95d4bef746d9e15f4c79f9450526a0.webp?lk3s=a5d48078&nonce=36193&refresh_token=5fc3ee621e9ab5b488448a31d513923a&x-expires=1730552400&x-signature=a5xHHkABjOgDLDazclv7vbM4KmA%3D&shp=a5d48078&shcp=fdd36af4',
+    followRole: 0,
+    userBadges: [
+        {
+            type: 'privilege',
+            privilegeId: '7196929090442513157',
+            level: 3,
+            badgeSceneType: 10
+        }
+    ],
+    userSceneTypes: [10],
+    userDetails: {
+        createTime: '0',
+        bioDescription: '',
+        profilePictureUrls: [
+            'https://p9-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/cb95d4bef746d9e15f4c79f9450526a0~tplv-tiktok-shrink:72:72.webp?lk3s=a5d48078&nonce=25872&refresh_token=ba7ee0c3bc28d447b5afd099090f47e9&x-expires=1730552400&x-signature=wHORehC7xShLkRVOltXvi5%2BYdoo%3D&shp=a5d48078&shcp=fdd36af4',
+            'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cb95d4bef746d9e15f4c79f9450526a0.webp?lk3s=a5d48078&nonce=36193&refresh_token=5fc3ee621e9ab5b488448a31d513923a&x-expires=1730552400&x-signature=a5xHHkABjOgDLDazclv7vbM4KmA%3D&shp=a5d48078&shcp=fdd36af4',
+            'https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cb95d4bef746d9e15f4c79f9450526a0.webp?lk3s=a5d48078&nonce=50620&refresh_token=1dfdc35ead6728a580bce23a218d39e6&x-expires=1730552400&x-signature=qWm221Wvf%2FwVv%2FDhEvAeYPgSrnY%3D&shp=a5d48078&shcp=fdd36af4',
+            'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cb95d4bef746d9e15f4c79f9450526a0.jpeg?lk3s=a5d48078&nonce=70606&refresh_token=893045a06d798f46b2185ad8ed5121b2&x-expires=1730552400&x-signature=atqpU3Q18gXUYtzVX0dakfIyWco%3D&shp=a5d48078&shcp=fdd36af4'
+        ]
+    },
+    followInfo: {
+        followingCount: 6,
+        followerCount: 19706,
+        followStatus: 0,
+        pushStatus: 0
+    },
+    isModerator: false,
+    isNewGifter: false,
+    isSubscriber: false,
+    topGifterRank: null,
+    gifterLevel: 0,
+    teamMemberLevel: 3,
+    createTime: '1730381643933',
+    msgId: '7431932543823792903'
+}
